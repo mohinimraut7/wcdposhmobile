0cfdfd40a85bdd6b217f6aabf63efafe3c05dd47
mohinimraut7 <mohinimraut7@gmail.com>
2026-06-30 13:02:58 +0530

    initial routes changed

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login"
+        initialRouteName="CompanyLogin"
         screenOptions={{headerShown: false}}>
         {/* Admin/Officer Login */}
         <Stack.Screen name="Login" component={LoginScreen} />
